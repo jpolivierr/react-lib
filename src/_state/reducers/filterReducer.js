e5fcd18989edcf1ea5077c70dd969c0b143b25c3
frederic <jpolivierr@gmail.com>
2023-02-04 00:23:09 -0500

    created redux store

--- src/_state/reducers/filterReducer.js
+++ src/_state/reducers/filterReducer.js
@@ -9,6 +9,8 @@ const filterReducer = (state = init, action) =>{
     switch(action.type){
         case SET_LOCATION :
             return {...state, location: action.payload}
+        default :
+            return state
 
     }
 }
---
+++ src/_state/reducers/index.js
@@ -0,0 +1,8 @@
+import filterReducer from "./filterReducer";
+import { combineReducers } from "redux";
+
+const reducers = combineReducers({
+    filterReducer
+})
+
+export default reducers
