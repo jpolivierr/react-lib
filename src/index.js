ac061d09c63fac532bfc646368a76787db10aef4
frederic <jpolivierr@gmail.com>
2023-03-11 19:57:54 -0500

    added Tele Neo Font

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import "./fonts/Inter Font Family/style.css"
+ import "./fonts/Inter Font Family/style.css"
 // import "./fonts/TT-Norms-Pro/style.css"
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
