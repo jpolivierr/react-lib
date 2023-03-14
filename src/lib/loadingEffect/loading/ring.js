8aa0168f1382b7cc070478736bb68fbea81f19d8
frederic <jpolivierr@gmail.com>
2023-03-14 12:37:38 -0400

    completed ring effect style

---
+++ src/lib/loadingEffect/loading/ring.js
@@ -0,0 +1,16 @@
+import "./style.css"
+import "./ring.css"
+
+const Ring = (props) =>{
+
+    const {isShowing} = props
+
+    return isShowing && (
+        <div className="av-loading">
+            <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+        </div>
+    )
+
+}
+
+export default Ring
